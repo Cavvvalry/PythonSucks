92e5c7ecdf5a8c3e36743710d193372647c3d61c
m3rc1l3ss <m3rc1l3ss@yahoo.com>
2016-04-05 22:56:14 -0700

    Code merge pt 1
    Added the parallax background to level 1
    Gave the player the ability to shift left and right
    Applied gravity to the player

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,13 +1,36 @@
+extends KinematicBody2D
 
-extends RigidBody2D
+var delay = 0
+var coll_obj
 
-# member variables here, example:
-# var a=2
-# var b="textvar"
+func _fixed_process(delta):
+    if ( Input.is_action_pressed("ui_left")):
+        move(Vector2(-5, 0))
+    if ( Input.is_action_pressed("ui_right")):
+        move(Vector2(5, 0))
+    if delay < 15:
+        get_node("Sprite").set_modulate(Color("ffffff"))
+    if delay > 0:
+        delay -= 1
+    if delay == 0:
+        set_layer_mask_bit(2, true)
+
+
+
+
 
 func _ready():
-	# Called every time the node is added to the scene.
-	# Initialization here
-	pass
+    set_fixed_process(true)
+
+
+func _on_bullet_body_enter( body ):
+    if ( delay == 0 ):
+        body.get_node("Sprite").set_modulate(Color("ff0000"))
+        set_layer_mask_bit(2, false)
+        delay = 60
+
+#func _on_bullet_body_exit( body ):
+#    print( "exited: ", body )
+#    body.get_node("Sprite").set_modulate(Color("ffffff"))
 
 
