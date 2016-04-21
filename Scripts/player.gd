517214baf2ddf81cb6ad697f1570de4588a3d0c4
m3rc1l3ss <m3rc1l3ss@yahoo.com>
2016-04-20 19:13:37 -0500

    Various stuff

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -9,6 +9,7 @@ var velocity = Vector2()
 var motion = Vector2()
 var health = 100
 var score = 0
+var jmp_detect = 0 
 
 func _fixed_process(delta):
 	_move_player(delta)
@@ -33,9 +34,17 @@ func _move_player(delta):
 	if velocity.x < -MOVE_SPEED:
 		velocity.x = -MOVE_SPEED
 	motion = velocity * delta
-	get_node("HUD ParaBKG/HUD ParaLYR/Label").set_text(str(score))
-	get_node("HUD ParaBKG/HUD ParaLYR/ProgressBar").set_value(health)
-	if (is_colliding()):
+	get_node("HUD ParaBKG/HUD ParaLYR/Score").set_text(str(score))
+	get_node("HUD ParaBKG/HUD ParaLYR/Health").set_value(health)
+	get_node("HUD ParaBKG/HUD ParaLYR/Health2").set_value(score)
+	print(score)
+	
+	if is_colliding():
+		jmp_detect = 0
+	else:
+		jmp_detect += 1 
+	
+	if (jmp_detect < 2):
 		var n = get_collision_normal()
 		motion = n.slide(motion)
 		move(motion)
@@ -46,13 +55,13 @@ func _move_player(delta):
 				score += 1
 		if ( Input.is_action_pressed("ui_left")):
 			left = true
-			animation_ctr += .2
+			animation_ctr += .1
 			velocity.x = -MOVE_SPEED
 			get_node("Run_Left").set_hidden(false)
 			get_node("Run_Left").set_frame(int(animation_ctr) % 4)
 		elif ( Input.is_action_pressed("ui_right")):
 			left = false
-			animation_ctr += .2
+			animation_ctr += .1
 			velocity.x = MOVE_SPEED
 			get_node("Run_Right").set_hidden(false)
 			get_node("Run_Right").set_frame(int(animation_ctr) % 4)
@@ -63,19 +72,18 @@ func _move_player(delta):
 			get_node("Sprite_Right").set_hidden(left)
 	else:
 		move(motion)
+		jmp_detect += 1
+		if jmp_detect > 2:
+			get_node("Run_Left").set_frame(0)
+			get_node("Run_Right").set_frame(0)
+			get_node("Run_Left").set_hidden(not left)
+			get_node("Run_Right").set_hidden(left)				
 		if ( Input.is_action_pressed("ui_left")):
 			left = true
 			velocity.x -= MOVE_SPEED * 0.1
-			get_node("Run_Left").set_hidden(false)
 		elif ( Input.is_action_pressed("ui_right")):
 			left = false
 			velocity.x += MOVE_SPEED * 0.1
-			get_node("Run_Right").set_hidden(false)
-		else:
-			animation_ctr = 0
-			get_node("Sprite_Left").set_hidden(not left)
-			get_node("Sprite_Right").set_hidden(left)
-
 
 
 
